ea3853521d4ce487d57da5c40c8ee6bdb380daaf
“samyakgaur” <samyakgaur@yahoo.com>
2021-12-07 02:39:51 +0530

    :sparkles: [Youtube API] Support added for youtube api

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# async-api-server
+# 📦 Youtube Lite
+## Follow what you like without getting distracted!
+
+Youtube Lite is a unique service enabling you to follow topics you like on youtube and get updated about the latest video on that topic, so that you can watch it in peace.
+
+- Simple and basic ✨
+- No more distractions 🧘🏼‍♂️
+
+## Features
+
+- 🚀 Get and search Paginated API Support.
+- 💡 Rotating API Keys for uninterrupted update!
+- 📝 Follow multiple topics.
+- 🏆 Robust search support
+
+What do people have to say about youtube?
+
+> Opening up YouTube can be a risky business. While YouTube can help you learn, it's also a site where clicking on one video can send you down a rabbit hole filled with screaming goats, mad music videos, and one extremely grumpy cat.
+
+
+## Tech
+
+- [Django] - Webserver.
+- [Django Rest Framework] - API Building.
+- [Celery] - Syncing data.
+- [Docker] - Deployment.
+- [Bootstrap] - Frontend.
+
+
+## Installation
+
+Clone the github repository.
+
+Run the following commands.
+
+```
+docker-compose build
+docker-compose up -d
+```
+All the required services will be up by now!
+
+After running the above commands:
+- Visit `http://127.0.0.1:8000/youtube/key/`
+        Add youtube API Keys. Make sure you add 2-3 API Keys to ensure uninterrupted feed.
+
+- Visit `http://127.0.0.1:8000/youtube/topic/`
+        Add a topic by clicking on `Add Topic` button. Then click on the `select` button to select the topic
+
+- Visit `http://127.0.0.1:8000/youtube/video/`
+   You should see the data flowing in!
+
+
+## API Support
+
+Youtube Lite also provides you with on-the-go API for Get and Search purpose.
+
+URL : `http://127.0.0.1:8000/youtube/api/video`
+Method: `GET`
+
+| Parameters | description |
+| ------ | ------ |
+| `page` | Jump to specific page |
+| `title` | Provide a search query |
+
+Note: Make sure you send in url encoded search query for accurate results.
--- youtube_api/views.py
+++ youtube_api/views.py
@@ -1,28 +1,17 @@
 from django.shortcuts import render, HttpResponseRedirect
-from datetime import datetime, timezone, timedelta
+from django.http import JsonResponse
+from django.db.models import Q
 from youtube_api.models import YoutubeAPI, YoutubeData, YoutubeTopic
-from youtube_api.api_call import fetch_data
-
-
-def test(request):
-    publishedAfter = get_start_time()
-    print(publishedAfter)
-    topic = YoutubeTopic.objects.filter(active=True).first()
-    key = YoutubeAPI.objects.first()
-    max_results = 50
-    order = "date"
-    data = fetch_data(key, order, topic, max_results, publishedAfter)
-    print(data.json())
-    return render(request, 'youtube_api/video_list.html', context={})
-
-
-def get_start_time():
-    utc_past_hour = datetime.utcnow() + timedelta(minutes=-3000000)
-    my_time = str(utc_past_hour.replace(tzinfo=timezone.utc)).split(' ')
-    return f"{my_time[0]}T{my_time[1][:-6]}Z"
+from youtube_api.api_call import fetch_data_wrapper
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+from youtube_api.serailizer import YoutubeDataSerializer
+from django.core.paginator import Paginator
 
 
 def youtube_topic(request):
+    '''Purpose: List view for saved topics.
+    It also handles creation of new topic.
+    '''
     if request.method == 'POST':
         topic = request.POST.get("youtube_topic", "")
         if topic != "":
@@ -35,6 +24,11 @@ def youtube_topic(request):
 
 
 def select_topic(request, topic_id):
+    '''Purpose: It selects a particular topic for feed.
+    It enables support for multiple topics for the user.
+    Cron Job: If there are no topics initially, we also start a async
+    job to keep updating the active topic feed.
+    '''
     selected_topic = YoutubeTopic.objects.filter(active=True)
     for topic in selected_topic:
         topic.active = False
@@ -42,10 +36,21 @@ def select_topic(request, topic_id):
     topic = YoutubeTopic.objects.get(id=topic_id)
     topic.active = True
     topic.save()
+    if not selected_topic:
+        # start sync because this is the first time any topic is selected
+        schedule, _ = IntervalSchedule.objects.get_or_create(
+                            every=1800, period=IntervalSchedule.SECONDS)
+        PeriodicTask.objects.create(
+            interval=schedule,
+            name='Sync Data',
+            task='youtube_api.api_call.sync_data')
+    fetch_data_wrapper()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 def youtube_api_key(request):
+    '''Purpose: API Key create and view.
+    '''
     if request.method == 'POST':
         key = request.POST.get("api_key", "")
         if key != "":
@@ -58,9 +63,62 @@ def youtube_api_key(request):
 
 
 def youtube_video_list(request):
+    ''' Purpose: This is a list view for the user youtube data.
+    It also has a search feature so that user can search a particular
+    video of his choice.
+    '''
     topic = YoutubeTopic.objects.filter(active=True).first()
-    videos = YoutubeData.objects.filter(topic=topic)
+    search_q = request.GET.get('search_q')
+    if search_q is not None:
+        lookup = Q(topic=topic) and (
+            Q(title__icontains=search_q) | Q(description__icontains=search_q))
+        videos = YoutubeData.objects.filter(lookup)
+    else:
+        videos = YoutubeData.objects.filter(topic=topic)
     context = {
         'videos': videos
     }
     return render(request, 'youtube_api/video_list.html', context)
+
+
+def youtube_video_list_api(request):
+    ''' Purpose: API endpoint for api services.
+    It gives user to search based on title/ description.
+    It supports pagination.
+    '''
+    topic = YoutubeTopic.objects.filter(active=True).first()
+    title_q = request.GET.get('title')
+    page_number = request.GET.get('page')
+    if page_number is None:
+        page_number = 1
+    else:
+        page_number = int(page_number)
+    try:
+        # Queryset building
+        if title_q is not None:
+            lookup = Q(topic=topic) and (
+                Q(title__icontains=title_q) | Q(
+                    description__icontains=title_q))
+        else:
+            lookup = Q(topic=topic)
+        search_results = YoutubeData.objects.filter(
+                            lookup).order_by('-published_at')
+
+        # Pagination
+        paginator = Paginator(search_results, 25)
+        page_obj = paginator.get_page(page_number)
+        if page_number > paginator.num_pages:
+            page_number = paginator.num_pages
+
+        # Serializer
+        serialized_results = YoutubeDataSerializer(
+                                page_obj.object_list, many=True)
+
+        return JsonResponse(
+            {"total pages": paginator.num_pages,
+             "page": page_number,
+             "response": serialized_results.data
+             })
+    except Exception as e:
+        print(e)
+        return JsonResponse({"success": "failed", "result": e})
