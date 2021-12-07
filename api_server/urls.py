14b43962ed655704e9c58ea8e1a4f7bb9c3abd24
“samyakgaur” <samyakgaur@yahoo.com>
2021-12-07 13:37:32 +0530

    :wrench: [Docker] Celery-Beat: Restart policy for celery-beat

--- api_server/urls.py
+++ api_server/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from youtube_api.views import youtube_topic
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('youtube/', include('youtube_api.urls')),
+    path('', youtube_topic, name='home'),
 ]
